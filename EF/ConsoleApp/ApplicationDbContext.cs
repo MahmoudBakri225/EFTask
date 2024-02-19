753aa6ca212f931e20f849a92d85d726009142e6
MahmoudBakri225 <mahmoudbakri225@gmail.com>
2024-02-20 00:49:37 +0200

    first commit

---
+++ EF/ConsoleApp/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp.configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=EFCore;Integrated Security=True;TrustServerCertificate=True");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            new TaskEntityTypeConfiguration().Configure(modelBuilder.Entity<Task>());
+
+
+
+        }
+    }
+}
---
+++ EF/ConsoleApp/configuration/TaskEntityTypeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.configuration
+{
+    internal class TaskEntityTypeConfiguration : IEntityTypeConfiguration<Task>
+    {
+        public void Configure(EntityTypeBuilder<Task> builder)
+        {
+            builder.Property(t => t.Title).IsRequired();
+            builder.ToTable("NewTask");
+            builder.Property(t => t.Date).HasColumnName("Deadline");
+        }
+    }
+}
